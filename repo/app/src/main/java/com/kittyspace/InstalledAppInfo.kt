03d566d2b89280dfafff95b5015441f63850de31
xcaretsilver-cmyk <xcaretsilver@gmail.com>
2026-06-01 06:52:43 +0100

    Delete repo directory

--- repo/app/src/main/java/com/kittyspace/InstalledAppInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.kittyspace
-
-data class InstalledAppInfo(
-    val packageName: String,
-    val appName: String,
-    val sourceDir: String,
-    val splitSourceDirs: List<String>?,
-    val isSystem: Boolean
-)
